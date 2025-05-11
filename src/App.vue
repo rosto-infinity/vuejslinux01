efe4e8184f65d0141f7046ff8dfaa6f8f7fa50e7
ROSTAND LELE <rostoinfocus@gmail.com>
2025-05-11 17:01:58 +0100

    print conditionnel

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <script setup>
  import {ref,} from 'vue'
- import stocksGreenImage from './assets/images/socks_green.jpg'
+ import stocksGreenImage from './assets/images/socks_blue.jpg'
+import Other from './Other.vue'
 
  const product =ref('STOCKS prof')
- const description =ref('Lorem ipsum dolor ')
+ const description =ref('Lorem ipsum dolor  Lorem ipsum dolorLorem ipsum dolorLorem ipsum dolorLorem ipsum dolorLorem ipsum dolorLorem ipsum dolor')
 const image =ref(stocksGreenImage)
+
+const inStock =ref(true)
+const inventory =ref(8)
 
 </script>
 <template>
@@ -12,17 +16,22 @@ const image =ref(stocksGreenImage)
 <div class="product-display">
   <div class="product-container">
     <div class="product-image">
-      <!-- image goes here -->
-       <img :src="image" alt="stok green">
-
-       
+      <!-- image goes herennh -->
+       <img :src="image" alt="stok green">  
     </div>
     <div class="product-info">
       <h1>{{ product }}</h1>
-      <p> {{ description }}</p>
+      <!-- <p> {{ description }}</p> -->
+      <hr class="mb-4">
+      <p v-show="inStock">In Stock</p>
+      <p v-if="inventory > 10">In Stock 2</p>
+      <p v-else-if="inventory <10 && inventory >0">Almost of Stock ==></p>
+      <p v-else>Out of Stock</p>
+      <!-- <p v-else>Out of Stock</p> -->
     </div>
   </div>
 </div>
+<Other/>
 </template>
 <style scoped>
 </style>
---
+++ src/Other.vue
@@ -0,0 +1,12 @@
+<script setup>
+import stocksProductsImages from './assets/images/socks_green.jpg'
+const props = defineProps({
+  product: {
+    type: Object,
+    default: () => ({ image: stocksProductsImages, name: "Produit" }),
+  },
+});
+</script>
+<template>
+  <img :src="product.image" :alt="`Image de ${product.name}`" />
+</template>
