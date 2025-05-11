6c00ebb24238a2ff05e9e121c19618d87127538d
ROSTAND LELE <rostoinfocus@gmail.com>
2025-05-11 13:12:56 +0100

    appelle de la reference pour demander sa valeur

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <script setup>
-
+ import {ref,} from 'vue'
+ const product =ref('STOCKS prof')
+ const description =ref('Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi veniam hic quia eius debitis perspiciatis voluptas quasi nesciunt sed tenetur? Perspiciatis fuga alias sequi quae fugit similique nulla illo laborum?')
 </script>
 
 <template>
   <div class="product-display">
     <div class="product-container">
       <div class="product-info text-center pt-20">
-        <h1 class="text-8xl ">Socks</h1>
+        <h1 class="text-8xl ">{{ product }}</h1>
+        <p> {{ description }}</p>
+        
       </div>
     </div>
   </div>
