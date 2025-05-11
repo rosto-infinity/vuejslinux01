c0c4c69edbe8d032a7a412bdcbecb7c16353a980
ROSTAND LELE <rostoinfocus@gmail.com>
2025-05-11 13:03:06 +0100

    config

--- src/App.vue
+++ src/App.vue
@@ -3,35 +3,13 @@
 </script>
 
 <template>
- <div class="text-2xl text-green-400 p-2">Rostand</div>
-  
+  <div class="product-display">
+    <div class="product-container">
+      <div class="product-info text-center pt-20">
+        <h1 class="text-8xl ">Socks</h1>
+      </div>
+    </div>
+  </div>
 </template>
-
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
