7c1de9b5a9bb9a6b6c5b04db4e4d848031721291
ROSTAND LELE <rostoinfocus@gmail.com>
2025-05-13 22:13:39 +0100

    color varint

--- src/EventCard.vue
+++ src/EventCard.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref } from 'vue'
 import socksGreenImage from './assets/images/socks_green.jpg'
+import socksBlueImage from './assets/images/socks_blue.jpg'
 
 const product = ref('Socks')
 const image = ref(socksGreenImage)
@@ -9,13 +10,17 @@ const inStock = true
 const details = ref(['50% cotton', '30% wool', '20% polyester'])
 
 const variants = ref([
-  { id: 2234, color: 'green' },
-  { id: 2235, color: 'blue' },
+  { id: 2234, color: 'green', image : socksGreenImage},
+  { id: 2235, color: 'blue',image : socksBlueImage},
 ])
 
 const cart = ref(0)
 const addToCart = () => {
       cart.value++
+}
+
+const UpdateImage = (variantImage) =>{
+  image.value =variantImage
 }
 </script>
   
@@ -31,14 +36,17 @@ const addToCart = () => {
         <h1>{{ product }}</h1>
         <p v-if="inStock">In Stock</p>
         <p v-else>Out of Stock</p>
-        <ul>
+        <ul class="bg-red-200 pl-4 mb-4">
           <li v-for="detail in details">{{ detail }}</li>
         </ul>
         <div
           v-for="variant in variants"
           :key="variant.id"
+          @mouseover="UpdateImage(variant.image)"
+          class="color-circle"
+          :style="{backgroundColor: variant.color}"
         >
-          {{ variant.color }}
+          <!-- {{ variant.color }} -->
         </div>
         <button @click="addToCart" class="button">Add to Cart</button>
       </div>
